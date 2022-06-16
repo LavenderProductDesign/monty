92815464508554e95d9984782c27bb7b24833871
Motlamelle <lebomotlamelle@gmail.com>
2022-06-16 22:18:20 +0200

    update main

--- main.c
+++ main.c
@@ -8,11 +8,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-char **op_toks = NULL;
-
-/*
- * main - the entry point for Monty Interp
- *
+/**
+ * main - entry point for Monty Interp
  * @argc: the count of arguments passed to the program
  * @argv: pointer to an array of char pointers to arguments
  *
